a3897e636cb5f8a804012460b68d203bde3b9a38
Rakshitha Dorairaj <rakshitha.dorai@gmail.com>
2025-08-24 15:20:38 +0530

    Update Pattern9.py

--- Week 2/Patterns/Pattern9.py
+++ Week 2/Patterns/Pattern9.py
@@ -1,1 +1,21 @@
+Problem link - https://www.geeksforgeeks.org/problems/triangle-pattern-1661718013/1?page=1&category=pattern-printing&sortBy=submissions
+Go ahead and solve the problem in GFG as well.
 
+n = 4;
+for i in range(1,n+1) :
+    for j in range (1,i+1):
+        print("*",end=" ");
+    print("")
+for i in range(1,n+1):
+    for j in range(1,n-i+2-1):
+        print("*", end=" ")
+    print("")
+
+Output:
+*
+* *
+* * *
+* * * *
+* * *
+* *
+*

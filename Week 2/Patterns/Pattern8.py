ae6cc23f3746922c9043c5b61f845c541ec6ff56
Rakshitha Dorairaj <rakshitha.dorai@gmail.com>
2025-08-24 14:27:43 +0530

    Update Pattern8.py

--- Week 2/Patterns/Pattern8.py
+++ Week 2/Patterns/Pattern8.py
@@ -1,1 +1,25 @@
+Write a Program that takes input n and prints the below pattern and upload the code to your github repo.
+Sample 1:
+Input:
+n = 4
+Expected output:
+*
+**
+***
+****
+Sample 2
+Input
+n= 6
+*
+**
+***
+****
+*****
+******
 
+Solution:
+n = int(input("n:"))
+for i in range(1,n+1) :
+    for j in range (1,i+1):
+        print("*",end=" ");
+    print("")

924be2fb97868bf2271ae6d46ae0b68d6ce4597b
Rakshitha Dorairaj <rakshitha.dorai@gmail.com>
2025-08-24 13:40:26 +0530

    Create Pattern6.py

---
+++ Week 2/Patterns/Pattern6.py
@@ -0,0 +1,22 @@
+Write a Program which takes input n and prints the below pattern and upload the code to your github repo.
+Sample 1:
+Input:
+n = 4
+Expected output:
+****
+***
+**
+*
+  
+Sample 2
+Input:
+n= 6
+******
+*****
+****
+***
+**
+*
+Solution:
+
+n = int(input("Enter n: "))

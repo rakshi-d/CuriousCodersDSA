984646b63e18583d434db4d2b4d40ed9609455f7
Rakshitha Dorairaj <rakshitha.dorai@gmail.com>
2025-08-24 14:21:16 +0530

    Update Pattern7.py

--- Week 2/Patterns/Pattern7.py
+++ Week 2/Patterns/Pattern7.py
@@ -1,1 +1,26 @@
+Write a Program which takes input n and prints the below pattern and upload the code to your github repo.
+Sample 1:
+Input:
+n = 4
+Expected output:
+4321
+321
+21
+1
+Sample 2
+Input:
+n= 6
+654321
+54321
+4321
+321
+21
+1
 
+Solution:
+n = int(input("Enter n: "));
+
+for i in range(1, n+1):
+    for j in range(1, n-i+2):
+        print(n - j - i + 2, end=" ")   # print decreasing numbers
+    print("")

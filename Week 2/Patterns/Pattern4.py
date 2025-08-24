81a4def2c38699778f5cec308da4ddcb648de1f5
Rakshitha Dorairaj <rakshitha.dorai@gmail.com>
2025-08-24 14:32:07 +0530

    Update Pattern4.py

--- Week 2/Patterns/Pattern4.py
+++ Week 2/Patterns/Pattern4.py
@@ -1,3 +1,6 @@
+Problem link - 
+https://www.geeksforgeeks.org/problems/pattern-printing1347/1?page=1&category=pattern-printing&sortBy=submissions
+
 n = 4;
 for i in range(1,n+1) :
     for j in range (1,i+1):

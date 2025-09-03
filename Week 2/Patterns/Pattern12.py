e3eb2e5510ad732bfd119727a09f253189ffacd1
Rakshitha Dorairaj <rakshitha.dorai@gmail.com>
2025-09-03 19:01:43 +0530

    Update Pattern12.py

--- Week 2/Patterns/Pattern12.py
+++ Week 2/Patterns/Pattern12.py
@@ -1,1 +1,17 @@
+n=int(input())
+for i in range(1,n):
+    for j in range(1,n-i+1):
+        print(end="  ")
+    for j in range(1,2*i):
+        print("*",end=" ")
+    print(" ")
+for i in range(1, n+2):
+    for j in range(1, i):
+        print(end="  ")
+    for j in range(1,(2*n)-(2*i)+2):
+        print("*",end=" ")
+    print(" ")
+
+Output:
+
 

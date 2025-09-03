71527c3ecdc576ff273fd746bc99aa66cbec67fd
Rakshitha Dorairaj <rakshitha.dorai@gmail.com>
2025-09-03 18:51:38 +0530

    Update Pattern11.py

--- Week 2/Patterns/Pattern11.py
+++ Week 2/Patterns/Pattern11.py
@@ -1,1 +1,14 @@
+N=int(input())
+for i in range(1, N+2):
+    for j in range(1, i):
+        print(end="  ")
+    for j in range(1,(2*N)-(2*i)+2):
+        print("*",end=" ")
+    print(" ")
 
+Output:
+N=4
+* * * * * * *
+  * * * * *
+    * * *
+      *

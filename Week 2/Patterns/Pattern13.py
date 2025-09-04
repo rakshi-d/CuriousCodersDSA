c03e7515e6c32571b15d39d979098995d3915464
Rakshitha Dorairaj <rakshitha.dorai@gmail.com>
2025-09-04 13:24:17 +0530

    Update Pattern13.py

--- Week 2/Patterns/Pattern13.py
+++ Week 2/Patterns/Pattern13.py
@@ -1,3 +1,12 @@
+n = int(input("Enter number of rows: "))
+
+count = 1
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        print(count, end=" ")
+        count += 1
+    print()
+  
 OUTPUT:
 N = 4
 1

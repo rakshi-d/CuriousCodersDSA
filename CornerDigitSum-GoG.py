26e98901f348e43879415959b1a40464afef8899
Rakshitha Dorairaj <rakshitha.dorai@gmail.com>
2025-09-08 14:07:01 +0530

    Update CornerDigitSum-GoG.py

--- CornerDigitSum-GoG.py
+++ CornerDigitSum-GoG.py
@@ -13,3 +13,19 @@ class Solution:
             count += 1
             n = n // 10
         return sum_
+
+COMPILER SOLUTION:
+n=12345
+sum_ = n % 10
+count = 0
+
+while n > 0:
+    if n == n % 10 and count > 0:   # when n becomes the first digit
+        sum_ += n % 10
+    count += 1                      # increment count
+    n = n // 10                     # remove last digit
+
+print(sum_)   # final output
+
+OUTPUT:
+6
